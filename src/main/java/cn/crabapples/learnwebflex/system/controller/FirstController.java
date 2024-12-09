b533e3aaade84882b9623b69f00f03bd6483e9ff
mshe <crabapples.cn@gmail.com>
2024-12-09 10:12:48 +0800

    gitignore: 更新 .gitignore 文件以忽略所有 rebel.xml 文件 controller: 在 FirstController 中添加 ServerHttpRequest 和 ServerHttpResponse 参数
    - 修改 .gitignore 文件，添加 !**/rebel.xml 条目以忽略所有 rebel.xml 文件
    - 更新 FirstController 中的 hello 方法，添加 ServerHttpRequest 和 ServerHttpResponse 参数
    - 在 hello 方法中添加打印请求和响应对象的代码

--- src/main/java/cn/crabapples/learnwebflex/system/controller/FirstController.java
+++ src/main/java/cn/crabapples/learnwebflex/system/controller/FirstController.java
@@ -2,6 +2,8 @@ package cn.crabapples.learnwebflex.system.controller;
 
 import cn.crabapples.learnwebflex.base.ResponseDTO;
 import org.springframework.boot.actuate.web.exchanges.HttpExchange;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,8 +13,10 @@ import reactor.core.publisher.Mono;
 public class FirstController {
 
     @RequestMapping("/hello")
-    public Mono<ResponseDTO<Object>> hello(HttpExchange exchange) {
+    public Mono<ResponseDTO<Object>> hello(HttpExchange exchange, ServerHttpRequest request, ServerHttpResponse response) {
         System.err.println(exchange);
+        System.err.println(request);
+        System.err.println(response);
         return Mono.defer(() -> {
             return Mono.create((sink) -> {
                 sink.success(new ResponseDTO<>(""));
